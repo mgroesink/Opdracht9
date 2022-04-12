1981240e6ca4846ff6ae5f5d0524931adfd676d5
mgroesink <mroesink@rocvantwente.nl>
2022-04-12 11:36:15 +0200

    View added to context

---
+++ Opdracht9/Migrations/20220401081032_vwRooster.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Text;
+
+#nullable disable
+
+namespace Opdracht9.Migrations
+{
+    public partial class vwRooster : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //string view = "CREATE VIEW vwRooster";
+            //view += " AS";
+            //view += " SELECT k.Klas as [Klas], d.Afkorting as [Docent],";
+            //view += "  v.Code as [Vak], r.Lokaal, r.Dag , r.Uur";
+            //view += " FROM roosters r";
+            //view += " INNER JOIN docenten d ON r.DocentId = d.id";
+            //view += " INNER JOIN klassen k ON r.KlasId = k.Id";
+            //view += " INNER JOIN vakken v ON r.VakId = v.Id";
+
+            // StringBuilder is een betere oplossing omdat strings unmutable zijn
+            StringBuilder sb = new StringBuilder("CREATE VIEW vwRooster");
+            sb.Append(" AS");
+            sb.Append(" SELECT k.Klas as [Klas], d.Afkorting as [Docent],");
+            sb.Append("  v.Code as [Vak], r.Lokaal, r.Dag , r.Uur");
+            sb.Append(" FROM roosters r");
+            sb.Append(" INNER JOIN docenten d ON r.DocentId = d.id");
+            sb.Append(" INNER JOIN klassen k ON r.KlasId = k.Id");
+            sb.Append(" INNER JOIN vakken v ON r.VakId = v.Id");
+            string view = sb.ToString();
+
+            migrationBuilder.Sql(view);
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP VIEW vwRooster");
+        }
+    }
+}
---
+++ Opdracht9/Models/ViewModels/RoosterVM.cs
@@ -0,0 +1,12 @@
+﻿namespace Opdracht9.Models.ViewModels
+{
+    public class RoosterVM
+    {
+        public string Klas { get; set; } = string.Empty;
+        public string Docent { get; set; } = string.Empty;
+        public string Vak { get; set; } = string.Empty;
+        public string Lokaal { get; set; } = string.Empty;
+        public DayOfWeek Dag { get; set; }
+        public int Uur { get; set; }
+    }
+}
